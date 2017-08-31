6a69de03fbd77874ebc15bd41ced7e935a0f094e
cdj68765 <cdj68765@163.com>
2017-09-01 04:58:35 +0800

    09-1

--- GetMiMiAiBtData/Program.cs
+++ GetMiMiAiBtData/Program.cs
@@ -1,10 +1,14 @@
 ﻿using BetterHttpClient;
 using HtmlAgilityPack;
+using SocksSharp;
+using SocksSharp.Proxy;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -15,13 +19,49 @@ namespace GetMiMiAiBtData
 {
     static class Program
     {
-        /// <summary>
-        /// 应用程序的主入口点。
-        /// </summary>
+        class ProxyHtmlTextHandler : ProxyClientHandler<Socks5>
+        {
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+                var contentType = response.Content.Headers.ContentType;
+                contentType.CharSet = await getCharSetAsync(response.Content);
+                return response;
+            }
+
+            private async Task<string> getCharSetAsync(HttpContent httpContent)
+            {
+                var charset = httpContent.Headers.ContentType.CharSet;
+                if (!string.IsNullOrEmpty(charset))
+                    return charset;
+
+                var content = await httpContent.ReadAsStringAsync();
+                var match = Regex.Match(content, @"charset=(?<charset>.+?)""", RegexOptions.IgnoreCase);
+                if (!match.Success)
+                    return charset;
+                return match.Groups["charset"].Value;
+            }
+            public ProxyHtmlTextHandler(ProxySettings proxySettings) : base(proxySettings)
+            {
+            }
+        }
         [STAThread]
         static void Main()
         {
-            var Web = new HttpClient(new Proxy("127.0.0.1:7070")) {Encoding = Encoding.Default};
+            var Web = new BetterHttpClient.HttpClient(new Proxy("127.0.0.1:7070")) {Encoding = Encoding.Default};
+            var ProxySettings = new ProxySettings()
+            {
+                Host = "127.0.0.1",
+                Port = 7070
+            };
+
+
+            using (var httpClient = new System.Net.Http.HttpClient(new ProxyHtmlTextHandler(ProxySettings)))
+            {
+                var response = httpClient.GetStringAsync("https://www.google.co.jp/").Result;
+            }
+
+
             /*using (var St = new StreamWriter(new FileStream("Web.txt", FileMode.Create)))
             {
                 St.Write(Web.Get(@"http://www.mimirrr.com/forumdisplay.php?fid=55&page=1"));
@@ -58,7 +98,5 @@ namespace GetMiMiAiBtData
              Application.SetCompatibleTextRenderingDefault(false);
              Application.Run(new Form1());*/
         }
-
-
     }
 }
